aa97822eb28c8e0b57edaacc7a5b38d70389886a
JulieM-dev <pixy.julie@gmail.com>
2026-05-14 01:18:01 +0200

    init commit

---
+++ src/test/java/com/mongault/leetcodetraining/LeetCodeTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mongault.leetcodetraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeetCodeTrainingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
